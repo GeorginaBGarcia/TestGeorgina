2c4e45e681bfbc8aaac0bc4710998c7ac2c7efac
GeorginaBuwu <164585022+GeorginaBGarcia@users.noreply.github.com>
2024-03-22 19:00:16 -0600

    Initial commit

---
+++ Source/TestGeorgina/Private/projectils.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "projectils.h"
+
+// Sets default values
+Aprojectils::Aprojectils()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Aprojectils::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Aprojectils::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TestGeorgina/TestGeorginaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGeorginaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestGeorginaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestGeorginaGameMode();
+};
+
+
+
